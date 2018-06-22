1f6b02c47a5428f34adcca163501d7030ed9d03f
guaju <guaju@126.com>
2018-06-22 16:06:39 +0800

    腾讯地图marker_info_window_及点击事件

--- app/src/main/java/com/guaju/tecentnavdemo/HelloTecnetMapActivity.java
+++ app/src/main/java/com/guaju/tecentnavdemo/HelloTecnetMapActivity.java
@@ -3,17 +3,23 @@ package com.guaju.tecentnavdemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tencent.map.geolocation.TencentLocation;
 import com.tencent.map.geolocation.TencentLocationListener;
 import com.tencent.map.geolocation.TencentLocationManager;
 import com.tencent.map.geolocation.TencentLocationRequest;
+import com.tencent.tencentmap.mapsdk.maps.CameraUpdate;
+import com.tencent.tencentmap.mapsdk.maps.CameraUpdateFactory;
 import com.tencent.tencentmap.mapsdk.maps.LocationSource;
 import com.tencent.tencentmap.mapsdk.maps.MapView;
 import com.tencent.tencentmap.mapsdk.maps.TencentMap;
 import com.tencent.tencentmap.mapsdk.maps.UiSettings;
 import com.tencent.tencentmap.mapsdk.maps.model.BitmapDescriptorFactory;
+import com.tencent.tencentmap.mapsdk.maps.model.CameraPosition;
 import com.tencent.tencentmap.mapsdk.maps.model.LatLng;
 import com.tencent.tencentmap.mapsdk.maps.model.Marker;
 import com.tencent.tencentmap.mapsdk.maps.model.MarkerOptions;
@@ -56,6 +62,37 @@ public class HelloTecnetMapActivity extends AppCompatActivity implements Tencent
         mapUiSettings.setCompassEnabled(true);
 
         mapUiSettings.setMyLocationButtonEnabled(true);
+
+         //给marker添加点击事件
+
+        tencentMap.setOnMarkerClickListener(new TencentMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                if (marker.isInfoWindowShown()){
+                    marker.hideInfoWindow();
+                }else{
+                    marker.showInfoWindow();
+                }
+
+                return true;
+            }
+        });
+
+        //设置点击地图 隐藏infowindow方法
+        tencentMap.setOnTapMapViewInfoWindowHidden(true);
+
+        tencentMap.setOnInfoWindowClickListener(new TencentMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Toast.makeText(HelloTecnetMapActivity.this, marker.getTitle()+marker.getSnippet(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onInfoWindowClickLocation(int i, int i1, int i2, int i3) {
+
+            }
+        });
+
     }
 
     @Override
@@ -102,12 +139,14 @@ public class HelloTecnetMapActivity extends AppCompatActivity implements Tencent
 
     @Override
     public void onLocationChanged(TencentLocation tencentLocation, int error, String s) {
-        //是当定位位置发生变化时触发
+        //是当定位位置发生变化时触发  ：
         if (TencentLocation.ERROR_OK == error) {
+            //定位成功时 
             Log.e("GUAJU", "onLocationChanged: "+tencentLocation );
             // 定位成功
             Toast.makeText(this, "定位成功", Toast.LENGTH_SHORT).show();
             tencentMap.setLocationSource(this);
+            //展示定位位置 并且显示到屏幕中心位置
             showLocatePosition(tencentLocation);
 
             
@@ -118,17 +157,66 @@ public class HelloTecnetMapActivity extends AppCompatActivity implements Tencent
         locationManager.removeUpdates(this);
     }
 
+    private void addInfoWindow(final Marker marker) {
+        TencentMap.InfoWindowAdapter infoWindowAdapter = new TencentMap.InfoWindowAdapter() {
+
+            private TextView tv_des;
+            TextView tvTitle;
+
+            //返回View为信息窗自定义样式，返回null时为默认信息窗样式
+            @Override
+            public View getInfoWindow(final Marker arg0) {
+                // TODO Auto-generated method stub
+                if (arg0.equals(marker)) {
+                    LinearLayout custInfowindow = (LinearLayout) View.inflate(
+                            HelloTecnetMapActivity.this, R.layout.infowindow_tecent, null);
+                    tvTitle = (TextView)custInfowindow.findViewById(R.id.tv_title);
+                    tv_des = (TextView)custInfowindow.findViewById(R.id.tv_des);
+                    tvTitle.setText(arg0.getTitle());
+                    tv_des.setText(arg0.getSnippet());
+
+                    return custInfowindow;
+                }
+                return null;
+            }
+
+            //返回View为信息窗内容自定义样式，返回null时为默认信息窗样式
+            @Override
+            public View getInfoContents(Marker arg0) {
+                // TODO Auto-generated method stub
+                return null;
+            }
+        };
+
+        //设置信息窗适配器
+        tencentMap.setInfoWindowAdapter(infoWindowAdapter);
+
+
+    }
+
     private void showLocatePosition(TencentLocation tencentLocation) {
         //标注坐标
         LatLng latLng = new LatLng(tencentLocation.getLatitude(),tencentLocation.getLongitude());
         final Marker marker = tencentMap.addMarker(new MarkerOptions().
                 position(latLng).
-                title("北京").
-                snippet("DefaultMarker"));
+                title(tencentLocation.getName()).
+                snippet(tencentLocation.getAddress()));
 
         marker.setIcon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher));
         
-//        tencentMap.setMapCenterAndScale()
+        //显示到中心位置  参数2 是地图的缩放级别 ： 最小是3  最大是19
+        /*
+        参数1  中心点的经纬度
+        参数2  地图的缩放级别    3-19
+        参数3  旋转角的度数
+        参数4  倾斜角的度数
+         */
+        CameraPosition cameraPosition = new CameraPosition(new LatLng(tencentLocation.getLatitude(), tencentLocation.getLongitude()), 16.0f, 0.0f, 0.0f);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        tencentMap.moveCamera(cameraUpdate);
+        //定位成功之后，添加infowindow
+        addInfoWindow(marker);
+
     }
 
     @Override
